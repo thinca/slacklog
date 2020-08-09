a5b4aee0894c6d799ca1d1dede6508b53207aa68
Slack Log Generator <slacklog@vim-jp.org>
2020-08-09 19:30:06 +0900

    Add files

---
+++ files/FBB9V17S7/-.c
@@ -0,0 +1,6 @@
+/*
+ * Set the scriptID for an option, taking care of setting the buffer- or
+ * window-local value.
+ */
+    static void
+set_option_scriptID_idx(int opt_idx, int opt_flags, int id)
