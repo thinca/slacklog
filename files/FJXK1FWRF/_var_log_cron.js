cb8821ac4c3d2508cb740e19dd487a7361321f32
Slack Log Generator <slacklog@vim-jp.org>
2020-08-09 19:41:12 +0900

    Add files

---
+++ files/FJXK1FWRF/_var_log_cron.js
@@ -0,0 +1,3 @@
+May 30 12:01:00 ixeris /usr/sbin/cron[73847]: in try_dlopen(): /usr/lib/pam_nologin.so.6: (null): Undefined symbol "fstat@FBSD_1.5"
+May 30 12:01:00 ixeris /usr/sbin/cron[73847]: in openpam_load_module(): no pam_nologin.so found
+May 30 12:01:00 ixeris /usr/sbin/cron[73847]: (CRON) error (can't start PAM)
