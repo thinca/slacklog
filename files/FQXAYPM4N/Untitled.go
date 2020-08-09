aca4faa87a08e90f36b56b24081f647b990b7831
Slack Log Generator <slacklog@vim-jp.org>
2020-08-09 23:42:44 +0900

    Add files

---
+++ files/FQXAYPM4N/Untitled.go
@@ -0,0 +1,9 @@
+	outreq := req.Clone(ctx)
+	if req.ContentLength == 0 {
+		outreq.Body = nil // Issue 16036: nil Body for http.Transport retries
+	}
+	if outreq.Header == nil {
+		outreq.Header = make(http.Header) // Issue 33142: historical behavior was to always allocate
+	}
+
+	p.Director(outreq)
