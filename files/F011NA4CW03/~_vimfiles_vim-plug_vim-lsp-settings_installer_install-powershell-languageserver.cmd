0c8d07484bb4cba63a9c3b7a40b50ee6a3401def
Slack Log Generator <slacklog@vim-jp.org>
2020-08-09 23:42:04 +0900

    Add files

---
+++ files/F011NA4CW03/~_vimfiles_vim-plug_vim-lsp-settings_installer_install-powershell-languageserver.cmd
@@ -0,0 +1,14 @@
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100     9  100     9    0     0      9      0  0:00:01 --:--:--  0:00:01    13
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   622  100   622    0     0    622      0  0:00:01 --:--:--  0:00:01   971
+100  100k  100  100k    0     0  51200      0  0:00:02  0:00:02 --:--:--  128k
+Archive:  PowerShellEditorServices.zip
+  End-of-central-directory signature not found.  Either this file is not
+  a zipfile, or it constitutes one disk of a multi-part archive.  In the
+  latter case the central directory and zipfile comment will be found on
+  the last disk(s) of this archive.
+unzip:  cannot find zipfile directory in PowerShellEditorServices.zip,
+        and cannot find PowerShellEditorServices.zip.zip, period.
