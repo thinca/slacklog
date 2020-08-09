19246779f40ac05fa5c0075dc7c46a45eb503f72
Slack Log Generator <slacklog@vim-jp.org>
2020-08-09 23:42:49 +0900

    Add files

---
+++ files/FSB1TSCLR/Untitled.c
@@ -0,0 +1,6 @@
+/*
+** Many system calls are accessed through pointer-to-functions so that
+** they may be overridden at runtime to facilitate fault injection during
+** testing and sandboxing.  The following array holds the names and pointers
+** to all overrideable system calls.
+*/
